a1131edffe553548f17a0acd3c04155961f25afb
Joann Whang <joann.whang@gmail.com>
2022-01-18 16:45:04 -0600

    chore(client): setup empty file for RandomQuote component

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     // state
+    this.state = {
+      randomQuote: {}
+    }
     // method bindings
-    // this.state = { counter: 0 };
     // this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('/quotes')
+      .then((response) => {
+        console.log('componentDidMount response', response.data);
+        this.setState({ randomQuote: response.data });
+      })
+      .catch((error) => {
+        console.log('componentDidMount error', error);
+      })
   }
 
   render() {
